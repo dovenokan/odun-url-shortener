d82d6a02a7c3a31d27578253537591816c0b6177
Okan <42724589+dovenokan@users.noreply.github.com>
2022-01-27 00:02:41 +0300

    env authprovider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0Provider
+        domain={process.env.REACT_APP_OAUTH_DOMAIN}
+        clientId={process.env.REACT_APP_OAUTH_CLIENTID}
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
