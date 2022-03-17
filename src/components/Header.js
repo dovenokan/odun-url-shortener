934ce224ae2cc41b1f6646146be777f0058fe205
Okan <42724589+dovenokan@users.noreply.github.com>
2022-03-17 15:42:55 +0300

    unnecessary div is removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import {useNavigate} from 'react-router-dom'
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 function Header() {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-    return (
-        <>
-            <img onClick={()=>navigate("/")} className="w-20 mx-auto" src={require("./img/odun.png")} alt="odun" />
-            <h1 className="text-3xl font-medium text-gray-500">
-              ODUN URL SHORTENER
-            </h1>
-            <h2 className="text-2xl font-medium text-gray-800">
-              <span className="text-gray-400 rounded px-1">SHORTEN.</span>
-              <span className="text-gray-400 rounded px-1">VISIT.</span>
-              <span className="text-gray-400 rounded px-1">UPDATE.</span>
-              <span className="block text-gray-400 text-sm rounded px-1">WE RESPECT YOUR PRIVACY</span>
-            </h2>
-        </>
-    )
+  return (
+    <>
+      <img onClick={()=>navigate("/")} className="w-20 mx-auto" src={require("./img/odun.png")} alt="odun" />
+      <h1 className="text-3xl font-medium text-gray-500">
+        ODUN URL SHORTENER
+      </h1>
+      <h2 className="text-2xl font-medium text-gray-800">
+        <span className="text-gray-400 rounded px-1">SHORTEN.</span>
+        <span className="text-gray-400 rounded px-1">VISIT.</span>
+        <span className="text-gray-400 rounded px-1">UPDATE.</span>
+        <span className="block text-gray-400 text-sm rounded px-1">WE RESPECT YOUR PRIVACY</span>
+      </h2>
+    </>
+  )
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-export default Header
+export default Header;
--- src/components/Table.js
+++ src/components/Table.js
@@ -8,64 +8,62 @@ import {copyText} from './Functions'
 function Table({data,update,del}) {
     return (
         <>
-        <div className="overflow-x-auto">
-            <div className="flex items-center justify-center font-sans">
-                <div className="">
-                <div className="bg-white shadow-md rounded my-6">
-                    <table className="table-auto">
-                    <thead>
-                        <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                            <th className="py-3 px-6 text-center">SHORT</th>
-                            <th className="py-3 px-6 text-center">COPY</th>
-                            <th className="py-3 px-6 text-center">URL</th>
-                            <th className="py-3 px-6 text-center">VISIT</th>
-                            <th className="py-3 px-6 text-center">UPDATE</th>
-                            <th className="py-3 px-6 text-center">DELETE</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-gray-600 text-sm font-light">
-                        {data.map((v) => {
-                            return(
-                                <tr key={v.ts} className="border-b border-gray-200 hover:bg-gray-100">
-                                    <td className="custom text-center py-3 px-6 text-center whitespace-nowrap">
-                                        <div className="text-center">
-                                            <span className="font-medium">{v.custom}</span>
-                                        </div>
-                                    </td>
-                                    <td className="copy py-3 px-6 text-center">
-                                        <div className="cursor-pointer hover:bg-gray-200 text-gray-600 px-1 rounded-full text-xs">
-                                            <span onClick={()=>copyText(`${window.origin}/${v.custom}`)}> <i className="fa fa-copy"></i> </span>
-                                        </div>
-                                    </td>
-                                    <td className="url py-3 px-6 text-center">
-                                        <div className="text-center">
-                                            <a target="blank" href={v.url}> <i className="fa fa-eye"></i> </a>
-                                        </div>
-                                    </td>
-                                    <td className="visit py-3 px-6 text-center">
-                                        <div className="text-center">
-                                            <span className="font-medium">{v.visit}</span>
-                                        </div>
-                                    </td>
-                                    <td className="update py-3 px-6 text-center">
-                                        <div className="text-center">
-                                            <span onClick={(e) => update(e,v)} className="cursor-pointer bg-gray-200 text-gray-600 py-1 px-3 rounded-full text-xs">Update</span>
-                                        </div>
-                                    </td>
-                                    <td className="delete py-3 px-6 text-center">
-                                        <div className="text-center">
-                                            <span onClick={(e) => del(e,v)} className="cursor-pointer bg-gray-200 text-gray-600 py-1 px-3 rounded-full text-xs">Delete</span>
-                                        </div>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                    </table>
-                </div>
+            <div className="overflow-x-auto">
+                <div className="flex items-center justify-center font-sans">
+                    <div className="bg-white shadow-md rounded my-6">
+                        <table className="table-auto">
+                        <thead>
+                            <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                <th className="py-3 px-6 text-center">SHORT</th>
+                                <th className="py-3 px-6 text-center">COPY</th>
+                                <th className="py-3 px-6 text-center">URL</th>
+                                <th className="py-3 px-6 text-center">VISIT</th>
+                                <th className="py-3 px-6 text-center">UPDATE</th>
+                                <th className="py-3 px-6 text-center">DELETE</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-gray-600 text-sm font-light">
+                            {data.map((v) => {
+                                return(
+                                    <tr key={v.ts} className="border-b border-gray-200 hover:bg-gray-100">
+                                        <td className="custom text-center py-3 px-6 text-center whitespace-nowrap">
+                                            <div className="text-center">
+                                                <span className="font-medium">{v.custom}</span>
+                                            </div>
+                                        </td>
+                                        <td className="copy py-3 px-6 text-center">
+                                            <div className="cursor-pointer hover:bg-gray-200 text-gray-600 px-1 rounded-full text-xs">
+                                                <span onClick={()=>copyText(`${window.origin}/${v.custom}`)}> <i className="fa fa-copy"></i> </span>
+                                            </div>
+                                        </td>
+                                        <td className="url py-3 px-6 text-center">
+                                            <div className="text-center">
+                                                <a target="blank" href={v.url}> <i className="fa fa-eye"></i> </a>
+                                            </div>
+                                        </td>
+                                        <td className="visit py-3 px-6 text-center">
+                                            <div className="text-center">
+                                                <span className="font-medium">{v.visit}</span>
+                                            </div>
+                                        </td>
+                                        <td className="update py-3 px-6 text-center">
+                                            <div className="text-center">
+                                                <span onClick={(e) => update(e,v)} className="cursor-pointer bg-gray-200 text-gray-600 py-1 px-3 rounded-full text-xs">Update</span>
+                                            </div>
+                                        </td>
+                                        <td className="delete py-3 px-6 text-center">
+                                            <div className="text-center">
+                                                <span onClick={(e) => del(e,v)} className="cursor-pointer bg-gray-200 text-gray-600 py-1 px-3 rounded-full text-xs">Delete</span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
